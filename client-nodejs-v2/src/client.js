92f5708ee58387499bbd44cbdd0aee6bbfea536f
MichaelVL <michael.larsen@eficode.com>
2021-07-20 14:17:12 +0200

    Swtich from mustache to ejs templating

--- client-nodejs-v2/src/client.js
+++ client-nodejs-v2/src/client.js
@@ -1,5 +1,5 @@
 const os = require("os");
-const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const redis = require('redis')
@@ -7,17 +7,16 @@ const redis = require('redis')
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const mustache = require('mustache');
 const { Issuer, Strategy } = require('openid-client');
 
 const client_title = process.env.CLIENT_TITLE || 'Confidential Client';
 const client_stylefile = process.env.CLIENT_STYLEFILE || 'style.css';
 const port = process.env.CLIENT_PORT || 5000;
 const base_url = process.env.CLIENT_BASE_URL || 'http://localhost:' + port
-const app = express();
 
-const template_index = fs.readFileSync('src/views/index.html', 'utf-8');
-const template_token = fs.readFileSync('src/views/token.html', 'utf-8');
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
@@ -38,12 +37,12 @@ if (redis_url) {
     const RedisStore = require('connect-redis')(session)
     const redisClient = redis.createClient({ url: redis_url })
     app.use(session({ secret: "mySessionSecret",
-		      resave: false, saveUninitialized: false,
-		      store: new RedisStore({ client: redisClient }), }));
+                      resave: false, saveUninitialized: false,
+                      store: new RedisStore({ client: redisClient }), }));
 } else {
     console.log('Using Memory session store');
     app.use(session({ secret: "mySessionSecret",
-		      resave: false, saveUninitialized: false}));
+                      resave: false, saveUninitialized: false}));
 }
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
@@ -54,113 +53,115 @@ app.use(passport.session());
 function isLoggedIn(req, res, next) {
     console.log('Check login of user', req.user);
     if ( ! req.user) {
-	console.log('User not logged in');
-	return res.redirect(base_url);
+        console.log('User not logged in');
+        return res.redirect(base_url);
     }
     next();
 }
 
 // Serve login page
 app.get('/', (req, res) => {
-    res.send(mustache.render(template_index, {'client_title': client_title,
-					      'client_title2': ' ('+hostname+' @ '+Date.now()+')',
-					      'client_stylefile': client_stylefile,
-					      'client_id': client_id,
-					      'oidc_issuer_url': oidc_issuer_url}));
+    now = new Date(Date.now());
+    res.render('index', {'client_title': client_title,
+                         'client_title2': ' ('+hostname+' @ '+now.toISOString()+')',
+                         'client_stylefile': client_stylefile,
+                         'client_id': client_id,
+                         'oidc_issuer_url': oidc_issuer_url});
 });
 
 Issuer.discover(oidc_issuer_url)
     .then(function (issuer) {
-	console.log('Discovered issuer %s %O', issuer.issuer, issuer.metadata);
+        console.log('Discovered issuer %s %O', issuer.issuer, issuer.metadata);
 
-	// Client settings for authorization code flow
-	var client = new issuer.Client({
-	    client_id: client_id,
-	    client_secret: client_secret,
-	    usePKCE: false,
-	    redirect_uris: [base_url+'/callback'],
-	    response_types: ['code'],
-	    token_endpoint_auth_method: 'client_secret_post'
-	});
+        // Client settings for authorization code flow
+        var client = new issuer.Client({
+            client_id: client_id,
+            client_secret: client_secret,
+            usePKCE: false,
+            redirect_uris: [base_url+'/callback'],
+            response_types: ['code'],
+            token_endpoint_auth_method: 'client_secret_post'
+        });
 
-	// Validation strategy
-	passport.use('oidc',
-		     new Strategy({ client }, (tokenSet, userinfo, done) => {
-			 // 'user' is a composite object with userinfo, tokens and claims
-			 return done(null, {userinfo: userinfo, claims: tokenSet.claims(), tokenSet: tokenSet});
-		     })
-		    );
+        // Validation strategy
+        passport.use('oidc',
+                     new Strategy({ client }, (tokenSet, userinfo, done) => {
+                         // 'user' is a composite object with userinfo, tokens and claims
+                         return done(null, {userinfo: userinfo, claims: tokenSet.claims(), tokenSet: tokenSet});
+                     })
+                    );
 
-	passport.serializeUser(function(user, done) {
-	    done(null, user);
-	});
-	passport.deserializeUser(function(user, done) {
-	    done(null, user);
-	});
+        passport.serializeUser(function(user, done) {
+            done(null, user);
+        });
+        passport.deserializeUser(function(user, done) {
+            done(null, user);
+        });
 
-	// Initiate authorization code flow using the 'oidc' strategy with customized scope
-	app.post('/login', (req, res, next) => {
-	    scope = req.body.scope;
-	    console.log('Requesting scope', scope);
-	    passport.authenticate('oidc', { scope: scope })(req, res, next);
-	});
+        // Initiate authorization code flow using the 'oidc' strategy with customized scope
+        app.post('/login', (req, res, next) => {
+            scope = req.body.scope;
+            console.log('Requesting scope', scope);
+            passport.authenticate('oidc', { scope: scope })(req, res, next);
+        });
 
-	// Authorization code flow callback
-	app.get('/callback', (req, res, next) => {
-	    passport.authenticate('oidc', {
-		successRedirect: '/user',
-		failureRedirect: '/'
-	    })(req, res, next);
-	});
+        // Authorization code flow callback
+        app.get('/callback', (req, res, next) => {
+            passport.authenticate('oidc', {
+                successRedirect: '/user',
+                failureRedirect: '/'
+            })(req, res, next);
+        });
 
-	// End session and redirect to login page
-	app.post('/logout', (req, res) => {
-	    req.session.destroy((err) => {
-		res.redirect(client.endSessionUrl({ id_token_hint: req.user.tokenSet.id_token,
-						    post_logout_redirect_uri: base_url}));
-	    });
-	});
+        // End session and redirect to login page
+        app.post('/logout', (req, res) => {
+            req.session.destroy((err) => {
+                res.redirect(client.endSessionUrl({ id_token_hint: req.user.tokenSet.id_token,
+                                                    post_logout_redirect_uri: base_url}));
+            });
+        });
 
-	// This actions checks login through a normal authorization code flow but with `prompt=none`
-	app.post('/refresh', (req, res) => {
-	    client.refresh(req.user.tokenSet.refresh_token).then(function (tokenSet) {
-		console.log('new tokens', tokenSet);
-		req.user.tokenSet = tokenSet;
-		res.redirect('/user');
-	    });
-	});
+        // This actions checks login through a normal authorization code flow but with `prompt=none`
+        app.post('/refresh', (req, res) => {
+            client.refresh(req.user.tokenSet.refresh_token).then(function (tokenSet) {
+                console.log('new tokens', tokenSet);
+                req.user.tokenSet = tokenSet;
+                res.redirect('/user');
+            });
+        });
 
-	// Show 'secret' information like tokens. Only shown to logged-in users
-	app.get('/user/', isLoggedIn, (req, res) => {
-	    console.log('User data', req.user);
-	    res.send(mustache.render(template_token, {'client_title': client_title,
-						      'client_title2': ' ('+hostname+' @ '+Date.now()+')',
-						      'client_stylefile': client_stylefile,
-						      'username': req.user.userinfo.preferred_username,
-						      'id_token': req.user.tokenSet.id_token,
-						      'id_token_claims': JSON.stringify(req.user.claims, null, '  '),
-						      'access_token':  req.user.tokenSet.access_token,
-						      'refresh_token':  req.user.tokenSet.refresh_token
-						     }));
-	    // setInterval(function () {
-	    // 	console.log('Check id token is still valid using introspection...');
-	    // 	client.introspect(req.user.tokenSet.id_token).then(function (token_status) {
-	    // 	    console.log('Token introspect result', token_status);
-	    // 	    if ( ! token_status.active ) {
-	    // 		console.log('Id token no longer active');
-	    // 	    }
-	    // 	})
-	    // }, 5000);
-	});
+        // Show 'secret' information like tokens. Only shown to logged-in users
+        app.get('/user/', isLoggedIn, (req, res) => {
+            now = new Date(Date.now());
+            console.log('User data', req.user);
+            res.render('token', {'client_title': client_title,
+                                 'client_title2': ' ('+hostname+' @ '+now.toISOString()+')',
+                                 'client_stylefile': client_stylefile,
+                                 'username': req.user.userinfo.preferred_username,
+                                 'id_token': req.user.tokenSet.id_token,
+                                 'id_token_claims': JSON.stringify(req.user.claims, null, '  '),
+                                 'access_token':  req.user.tokenSet.access_token,
+                                 'refresh_token':  req.user.tokenSet.refresh_token
+                                });
+            // setInterval(function () {
+            //  console.log('Check id token is still valid using introspection...');
+            //  client.introspect(req.user.tokenSet.id_token).then(function (token_status) {
+            //      console.log('Token introspect result', token_status);
+            //      if ( ! token_status.active ) {
+            //          console.log('Id token no longer active');
+            //      }
+            //  })
+            // }, 5000);
+        });
 
-	// Error handler
-	app.use(function(err, req, res, next) {
-	    console.log('Error handler', err);
-	    res.locals.message = err.message;
-	    res.locals.error = req.app.get('env') === 'development' ? err : {};
-	    res.status(err.status || 500);
-	    res.send(err);
-	});
+        // Error handler
+        app.use(function(err, req, res, next) {
+            console.log('Error handler', err);
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
+            res.status(err.status || 500);
+            res.send(err);
+        });
     });
 
 app.listen(port, () => {
--- client-nodejs/src/client.js
+++ client-nodejs/src/client.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const mustache = require('mustache');
 const crypto = require('crypto');
 const randomstring = require("randomstring");
 const querystring = require("querystring");
@@ -13,10 +13,10 @@ const client_title = process.env.CLIENT_TITLE || 'Confidential Client';
 const client_stylefile = process.env.CLIENT_STYLEFILE || 'style.css';
 const port = process.env.CLIENT_PORT || 5000;
 const base_url = process.env.CLIENT_BASE_URL || 'http://localhost:' + port
-const app = express();
 
-const template_index = fs.readFileSync('src/views/index.html', 'utf-8');
-const template_token = fs.readFileSync('src/views/token.html', 'utf-8');
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 var scope, state, nonce;
 var id_token, accesS_token, refresh_token;
@@ -42,24 +42,26 @@ app.use(express.static('src/static'));
 // redirect to the 'already logged-in' area
 app.get('/', (req, res) => {
     if (id_token_claims) {
-	return res.redirect(base_url+'/protected');
+        return res.redirect(base_url+'/protected');
     }
-    res.send(mustache.render(template_index, {'client_title': client_title,
-					      'client_stylefile': client_stylefile,
-					      'client_id': client_id,
-					      'oidc_auth_url': oidc_auth_url}));
+    res.render('index', {'client_title': client_title,
+                         'client_title2': '',
+                         'client_stylefile': client_stylefile,
+                         'client_id': client_id,
+                         'oidc_auth_url': oidc_auth_url});
 });
 
 // Show 'secret' information like tokens. This client is not secure and stores login-state
 // globally, so this is only for  demonstration purposes
 app.get('/protected/', (req, res) => {
-    res.send(mustache.render(template_token, {'client_title': client_title,
-					      'client_stylefile': client_stylefile,
-					      'username': id_token_claims.preferred_username,
-					      'id_token': id_token,
-					      'id_token_claims': JSON.stringify(id_token_claims, null, '  '),
-					      'access_token': access_token,
-					      'refresh_token': refresh_token }));
+    res.render('token', {'client_title': client_title,
+                         'client_title2': '',
+                         'client_stylefile': client_stylefile,
+                         'username': id_token_claims.preferred_username,
+                         'id_token': id_token,
+                         'id_token_claims': JSON.stringify(id_token_claims, null, '  '),
+                         'access_token': access_token,
+                         'refresh_token': refresh_token });
 });
 
 // First step in an authorization code flow login. Redirect to Identity provider (IdP).
@@ -70,12 +72,12 @@ app.post('/login', (req, res) => {
     console.log('Using scope', scope, 'state', state, 'nonce', nonce);
 
     let url = oidc_auth_url + '?' + querystring.encode({
-	'response_type': 'code',               // Use authorization code flow
-	'client_id': client_id,                // This is who we are
-	'scope': scope,                        // What we 'want'
-	'redirect_uri': base_url+'/callback',  // Call us here when login done at IdP
-	'state': state,                        // For our own use, if we need it and for protection
-	'nonce': nonce                         // Replay protection, echoed in id_token
+        'response_type': 'code',               // Use authorization code flow
+        'client_id': client_id,                // This is who we are
+        'scope': scope,                        // What we 'want'
+        'redirect_uri': base_url+'/callback',  // Call us here when login done at IdP
+        'state': state,                        // For our own use, if we need it and for protection
+        'nonce': nonce                         // Replay protection, echoed in id_token
     });
     console.log('Redirecting login to identity provider', url);
     res.redirect(url);
@@ -90,62 +92,62 @@ app.get('/callback', (req, res) => {
     console.log('Callback with code', code, 'state', idp_state, 'our state is', state);
 
     if ( ! code) {
-	error = req.query.error;
-	console.log('No code returned. Error', error);
-	id_token = null;
-	id_token_claims = null;
-	access_token = null;
-	refresh_token = null;
-	return res.redirect(base_url);
+        error = req.query.error;
+        console.log('No code returned. Error', error);
+        id_token = null;
+        id_token_claims = null;
+        access_token = null;
+        refresh_token = null;
+        return res.redirect(base_url);
     }
 
     if (state != idp_state) {
-	console.log('Error, state mismatch. We do not know this callback.');
-	return res.redirect(base_url);
+        console.log('Error, state mismatch. We do not know this callback.');
+        return res.redirect(base_url);
     }
 
     // This is a confidential client - authorize towards IdP with client id and secret
     const client_creds = 'Basic ' + Buffer.from(querystring.escape(client_id)+':'+querystring.escape(client_secret), 'ascii').toString('base64')
     const data = querystring.encode({
-	'code': code,
-	'grant_type': 'authorization_code',
-	'redirect_uri': base_url+'/callback'});
+        'code': code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': base_url+'/callback'});
     const options = {
-	method: 'POST',
-	headers: {
-	    'Authorization': client_creds,
-	    'Content-Type': 'application/x-www-form-urlencoded',
-	    'Content-Length': data.length
-	}
+        method: 'POST',
+        headers: {
+            'Authorization': client_creds,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': data.length
+        }
     };
 
     // Exchange code for tokens using the token endpoint
     const post = https.request(oidc_token_url, options, (post_resp) => {
-	console.log('statusCode:', post_resp.statusCode);
-	post_resp.on('data', (data) => {
-	    const token_data = JSON.parse(data);
-	    console.log('Token response', token_data);
-	    if (post_resp.statusCode == 200){
-		if (token_data.id_token) {
-		    id_token =  token_data.id_token;
-		    console.log('ID token', id_token);
+        console.log('statusCode:', post_resp.statusCode);
+        post_resp.on('data', (data) => {
+            const token_data = JSON.parse(data);
+            console.log('Token response', token_data);
+            if (post_resp.statusCode == 200){
+                if (token_data.id_token) {
+                    id_token =  token_data.id_token;
+                    console.log('ID token', id_token);
 
-		    // TODO: Validate signature on id_token
+                    // TODO: Validate signature on id_token
 
-		    id_token_claims = jwt_decode(id_token);
-		    console.log('ID token claims', id_token_claims);
-		}
-		if (token_data.access_token) {
-		    access_token = token_data.access_token
-		    console.log('Access token', access_token);
-		}		
-		if (token_data.refresh_token) {
-		    refresh_token = token_data.refresh_token
-		    console.log('Refresh token', refresh_token);
-		}		
-		res.redirect(base_url);
-	    }
-	});
+                    id_token_claims = jwt_decode(id_token);
+                    console.log('ID token claims', id_token_claims);
+                }
+                if (token_data.access_token) {
+                    access_token = token_data.access_token
+                    console.log('Access token', access_token);
+                }               
+                if (token_data.refresh_token) {
+                    refresh_token = token_data.refresh_token
+                    console.log('Refresh token', refresh_token);
+                }               
+                res.redirect(base_url);
+            }
+        });
     });
     post.write(data);
     post.end();
@@ -167,14 +169,14 @@ app.post('/checklogin', (req, res) => {
     console.log('Using scope', scope, 'state', state, 'nonce', nonce);
 
     let url = oidc_auth_url + '?' + querystring.encode({
-	'response_type': 'code',               // Use authorization code flow
-	'client_id': client_id,                // This is who we are
-	'scope': scope,                        // What we 'want'
-	'redirect_uri': base_url+'/callback',  // Call us here when login done at IdP
-	'state': state,                        // For our own use, if we need it and for protection
-	'nonce': nonce,                        // Replay protection, echoed in id_token
-	'id_token_hint': id_token,             // Check login for this identity
-	'prompt': 'none'                       // Don't query user for username/password
+        'response_type': 'code',               // Use authorization code flow
+        'client_id': client_id,                // This is who we are
+        'scope': scope,                        // What we 'want'
+        'redirect_uri': base_url+'/callback',  // Call us here when login done at IdP
+        'state': state,                        // For our own use, if we need it and for protection
+        'nonce': nonce,                        // Replay protection, echoed in id_token
+        'id_token_hint': id_token,             // Check login for this identity
+        'prompt': 'none'                       // Don't query user for username/password
     });
     console.log('Redirecting login to identity provider', url);
     res.redirect(url);
