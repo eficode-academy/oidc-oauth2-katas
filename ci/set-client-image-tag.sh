eeb63bee425c51452966218872ceee3cfc6598ba
MichaelVL <michael.larsen@eficode.com>
2021-10-07 06:56:59 +0200

    Add spa api gw to image update

--- ci/set-client-image-tag.sh
+++ ci/set-client-image-tag.sh
@@ -12,6 +12,6 @@ fi
 
 IMAGE=praqma/oidc-oauth2-katas-client
 
-for m in "kubernetes/client1.yaml" "kubernetes/client1-v2.yaml" "kubernetes/client1-v3.yaml" "kubernetes/client2.yaml" "kubernetes/hazard-service.yaml" "kubernetes/object-store-v2.yaml" "kubernetes/protected-api.yaml" "kubernetes/spa-cdn.yaml" "kubernetes/spa-login.yaml"; do
+for m in "kubernetes/client1.yaml" "kubernetes/client1-v2.yaml" "kubernetes/client1-v3.yaml" "kubernetes/client2.yaml" "kubernetes/hazard-service.yaml" "kubernetes/object-store-v2.yaml" "kubernetes/protected-api.yaml" "kubernetes/spa-cdn.yaml" "kubernetes/spa-login.yaml" "kubernetes/spa-api-gw.yaml"; do
     sed -i -E "s#(^\s+-\s+image\:\s+$IMAGE\:).*#\1$TAG#" $m
 done
